e66a59bfbe41f7f0c00ee4505e4c67ebc86c1273
Tiago Silva <tiagodasilva360@hotmail.com>
2021-08-24 19:43:14 -0300

    Abstração

--- EstudoTiago/OOP/02 - Pilares OOP/03 - Abstracao.cs
+++ EstudoTiago/OOP/02 - Pilares OOP/03 - Abstracao.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OOP._02___pilares_OOP
+namespace OOP
 {
-    class _03___Abstracao
+    public abstract class Eletrodomestico
     {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+        public abstract void Desligar();
     }
 }
