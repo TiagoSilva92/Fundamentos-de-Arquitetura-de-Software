5b1bc0452cdf180d8f9e95fb6c45a67648a8180d
Tiago Silva <tiagodasilva360@hotmail.com>
2021-08-24 19:37:04 -0300

    Herança

--- EstudoTiago/OOP/02 - Pilares OOP/02 - Heranca.cs
+++ EstudoTiago/OOP/02 - Pilares OOP/02 - Heranca.cs
@@ -2,9 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OOP._02___pilares_OOP
+namespace OOP
 {
-    class _02___Heranca
+    public class Funcionario : Pessoa
     {
+        public DateTime DataAdmissao { get; set; }
+
+        public string Registro { get; set; }
+    }
+
+    public class Processo 
+    { 
+        public void Execucao()
+        {
+            var funcionario = new Funcionario()
+            {
+                Nome = "João da Silva",
+                DataNascimento = Convert.ToDateTime("1990/01/01"),
+                DataAdmissao = DateTime.Now,
+                Registro = "0123456",
+            };
+
+            funcionario.CalcularIdade();
+        }
     }
 }
