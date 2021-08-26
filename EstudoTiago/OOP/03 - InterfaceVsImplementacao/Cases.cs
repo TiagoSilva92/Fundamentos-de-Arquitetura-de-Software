cd8fc061eebdae07f73b9929be69ae91a8067e85
Tiago Silva <tiagodasilva360@hotmail.com>
2021-08-26 19:52:48 -0300

    InterfaceVsComposicao

---
+++ EstudoTiago/OOP/03 - InterfaceVsImplementacao/Cases.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public interface IRepositorio
+    {
+        void Adicionar();
+    }
+
+    public class Repositorio : IRepositorio
+    {
+        public void Adicionar()
+        {
+
+        }
+    }
+
+    public class RepositorioFake : IRepositorio
+    {
+        public void Adicionar()
+        {
+
+        }
+    }
+
+    public class UsoImplementação
+    {
+        public void Processo()
+        {
+            var repositorio = new Repositorio();
+            repositorio.Adicionar();
+        }
+    }
+
+    public class UsoAbstracao
+    {
+        private readonly IRepositorio _repositorio;
+
+        public UsoAbstracao(IRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void Processo()
+        {
+            _repositorio.Adicionar();
+        }
+    }
+
+    public class TesteInterfaceImplementação
+    {
+        public TesteInterfaceImplementação()
+        {
+            var repImp = new UsoImplementação();
+            repImp.Processo();
+
+            var repAbs = new UsoAbstracao(new Repositorio());
+            repAbs.Processo();
+
+            var repAbsFake = new UsoAbstracao(new RepositorioFake());
+            repAbsFake.Processo();
+        }
+    }
+}
+
